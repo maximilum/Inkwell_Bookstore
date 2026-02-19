ab78af88acec826909f17e36e5fd25e0d70b7653
Mohammed Zain <87908878+maximilum@users.noreply.github.com>
2026-02-19 16:18:20 +0300

    create remaning book apis
    get book, get all books, delete book, edit book,

--- Backend/controllers/booksControllers.js
+++ Backend/controllers/booksControllers.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Book = require("../models/book.model");
 
-const postABook = async (req, res) => {
+const postBook = async (req, res) => {
   const book = req.body;
   try {
     const newBook = new Book(book);
@@ -19,4 +19,81 @@ const postABook = async (req, res) => {
   }
 };
 
-exports.postABook = postABook;
+const getBook = async (req, res) => {
+  const id = req.params.id;
+  const isValid = mongoose.Types.ObjectId.isValid(id);
+  if (!isValid) {
+    return res.status(400).json({ message: "Invalid book ID" });
+  }
+
+  try {
+    const book = await Book.findById(id);
+    if (!book) return res.status(404).json({ error: "Book not found" });
+    res.status(200).json({ success: true, data: book });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ success: false, message: `server error: ${error.message}` });
+  }
+};
+
+const getAllBooks = async (req, res) => {
+  try {
+    const books = await Book.find({}).sort({ createdAt: -1 });
+    if (!books) {
+      return res
+        .json(404)
+        .json({ success: true, message: "No books were found" });
+    }
+    return res.status(200).json({ success: true, data: books });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+const deleteBook = async (req, res) => {
+  const id = req.params.id;
+  const isValid = mongoose.Types.ObjectId.isValid(id);
+  if (!isValid) {
+    return res.status(400).json({ message: "Invalid book ID" });
+  }
+  try {
+    const deletedBook = await Book.findByIdAndDelete(id, { new: true });
+    if (!deletedBook) {
+      return res
+        .status(404)
+        .json({ success: false, message: "book not found" });
+    }
+    return res.status(200).json({ success: true, data: deletedBook });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+const editBook = async (req, res) => {
+  const update = req.body;
+  const id = req.params.id;
+  const isValid = mongoose.Types.ObjectId.isValid(id);
+  if (!isValid) {
+    return res.status(400).json({ message: "Invalid book ID" });
+  }
+  try {
+    const updatedBook = await Book.findByIdAndUpdate(id, update, { new: true });
+    if (!updatedBook) {
+      return res
+        .status(404)
+        .json({ success: false, message: "book not found" });
+    }
+    return res
+      .status(200)
+      .json({ success: true, message: "book updated", data: updatedBook });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+exports.postBook = postBook;
+exports.getBook = getBook;
+exports.getAllBooks = getAllBooks;
+exports.deleteBook = deleteBook;
+exports.editBook = editBook;
--- Backend/routers/booksRouter.js
+++ Backend/routers/booksRouter.js
@@ -1,9 +1,22 @@
 const express = require("express");
-const { postABook } = require("../controllers/booksControllers");
+const {
+  postBook,
+  getBook,
+  getAllBooks,
+  deleteBook,
+  editBook,
+} = require("../controllers/booksControllers");
 
 // Create Router
 const Router = express.Router();
 
-Router.post("/", postABook);
+Router.post("/", postBook);
 
+Router.get("/:id", getBook);
+
+Router.get("/", getAllBooks);
+
+Router.delete("/:id", deleteBook);
+
+Router.patch("/:id", editBook);
 exports.booksRouter = Router;
